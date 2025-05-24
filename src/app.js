06178551c52428a921a6e19c320d92157ec9dda6
Kiran Darekar <darekarkiran704@gmail.com>
2025-05-24 14:48:17 +0530

    exploring the routing

--- src/app.js
+++ src/app.js
@@ -3,17 +3,29 @@ const express = require('express');
 
 const app = express();
 
-app.use("/", (req, res)=>{
-    res.send('normal started responding');
+// app.use("/", (req, res)=>{
+//     res.send('hello 2 started responding');
+// });
+
+// this will only handle get call /user
+app.get("/user", (req, res)=>{
+    res.send({
+        firstName: "Kiran"
+    });
 });
 
-app.use("/hello", (req, res)=>{
-    res.send('hello server started responding');
+// this call will save data to post call /user
+app.post("/user", (req, res)=>{
+    console.log('saving the data to DB');
+    res.send("Data has been succefully save in DB");
 });
 
-app.use("/test", (req, res)=>{
-    res.send('server started responding');
+// this call will save data to post call /user
+app.delete("/user", (req, res)=>{
+    console.log('deleted the data from DB');
+    res.send("Data has been succefully deleted in DB");
 });
+
 
 app.listen(3000, ()=>{
     console.log('server is listening on 3000');

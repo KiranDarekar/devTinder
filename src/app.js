000ae1f92c4886f808ef65036aa8821e1af51137
Kiran Darekar <darekarkiran704@gmail.com>
2025-05-30 22:53:34 +0530

    write get user , feed API

--- src/app.js
+++ src/app.js
@@ -17,37 +17,62 @@ app.get('/admin/deleteAllUserData', (req, res, next) => {
     res.send('delete all user data sent');
 });
 
-app.get('/user', (req, res, next) => {
-    // try {
-        throw new Error('there is error in code');
-    // } catch (error) {
-    //     res.status(500).send("some error contact support team");
-    // }
-    
+// feed API  - get  all the user from deta database
+app.get('/feed', async (req, res) => {
+    try {
+        const userdetails = await User.find({});
+
+        if(userdetails.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(userdetails);
+        }
+        
+    } catch {
+        res.status(400).send("User not found");
+    }
+
+});
+
+// get user by email
+app.get('/user', async (req, res) => {
+    const userEmailId = req.body.emailId;
+    console.log(userEmailId);
+    try {
+        const userdetails = await User.findOne({ emailId : userEmailId });
+
+        if(userdetails.length === 0) {
+            res.status(404).send("User not found");
+        } else {
+            res.send(userdetails);
+        }
+        
+    } catch {
+        res.status(400).send("User not found");
+    }
+
 });
 
 app.post("/signup", async (req, res) => {
-    
-     const user = new User(req.body);
-
+    const user = new User(req.body);
     try {
         await user.save();
         res.send("user added succesfully !!!");
-    } catch(error){
+    } catch (error) {
         res.status(400).send("user added succesfully !!!");
     }
-    
+
 });
 
-app.use('/', (error, req, res, next) => {
-    if(error){
-        res.status(500).send("something went wrong");
-    }
-});
+// app.use('/', (error, req, res, next) => {
+//     if(error){
+//         res.status(500).send("something went wrong");
+//     }
+// });
 
-connectDb().then(() =>{
+connectDb().then(() => {
     console.log('Database has been successfuly connected.');
-    app.listen(3000, ()=>{
+    app.listen(3000, () => {
         console.log('server is listening on 3000');
     });
 }).catch((error) => {
